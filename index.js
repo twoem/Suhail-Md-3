c929382eb98103eee175614ee56131feda9af48f
Ha-Ngacha Cyber <twoemonline@gmail.com>
2025-05-12 17:20:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const start = async () => {
     bot.logger.info('⏳ Database syncing!')
     await bot.DATABASE.sync()
     await bot.connect()
+const express = require("express");
+const app = express();
+
+// Your actual web app or bot code
+app.get("/", (req, res) => {
+  res.send("App is alive!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
+
   } catch (error) {
     Debug.error(error);
     start();
